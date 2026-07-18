144741dbd556b73ae005a73799d8eec78402a10c
IvanaFD <ivanafigueroa0936@gmail.com>
2026-07-17 23:53:40 -0600

    Conecta render con el Game of Life

--- src/render.rs
+++ src/render.rs
@@ -1,25 +1,7 @@
-use raylib::prelude::*;
 use crate::framebuffer::Framebuffer;
-use crate::line::line;
+use crate::life::GameOfLife;
 
-
-pub fn render(
-    framebuffer: &mut Framebuffer,
-    traslate_x: f32,
-    traslate_y: f32,
-){
-    framebuffer.set_current_color(Color::GREEN);
-    line(
-        framebuffer,
-        Vector2::new(50.0 + traslate_x, 50.0 + traslate_y),
-        Vector2::new(350.0 + traslate_x, 350.0 + traslate_y),
-    );
-
-    framebuffer.set_current_color(Color::RED);
-    line(
-        framebuffer,
-        Vector2::new(350.0 + traslate_x, 50.0 + traslate_y),
-        Vector2::new(50.0 + traslate_x, 350.0 + traslate_y),
-    );
-
+pub fn render(framebuffer: &mut Framebuffer, grid: &mut GameOfLife) {
+    grid.draw(framebuffer);
+    *grid = grid.step();
 }
